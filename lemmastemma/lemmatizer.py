bb8b07a70ae80263021543fce38e09a2fc262479
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-06-14 14:36:36 +0100

    New lemmatizer
    Now based on Spacy.
    
    Uses the transformer model, which takes some time to load, adding
    considerable startup cost. For this reason I've made two unhappy
    hacks in the previous "small independent tools running in parallel"
    approach:
    1) Built in an optional entity stripper within the lemmatizer,
    rather than doubling the size of the problem by having two tools
    loading the model per file. Required a makefile hack.
    2) Hacked the makefile to build lemmatized target as a group.
    
    An alternative to this would be to not use the transformer model,
    but I shall stick with it for now.

--- lemmastemma/lemmatizer.py
+++ lemmastemma/lemmatizer.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
-#re https://stackoverflow.com/a/22283803
+#re https://spacy.io/usage/spacy-101#annotations-pos-deps
+#python -m spacy download en_core_web_trf
 
-import sys
-import nltk
-from nltk.stem import WordNetLemmatizer
+import spacy
+import argparse
 
-wnl = WordNetLemmatizer()
-for in_fnam in sys.argv[1:]:
+parser = argparse.ArgumentParser()
+parser.add_argument('infiles', nargs = '*', type = str, help = 'File(s) to process')
+parser.add_argument('--strip-entities', action = argparse.BooleanOptionalAction, default = False, help = 'Drop all named entities')
+args = parser.parse_args()
+
+spacy.require_gpu()
+nlp = spacy.load("en_core_web_trf")
+nlp.max_length = 3000000 #Alarmingly high -- may consume ~30GB of RAM!
+
+for in_fnam in args.infiles:
   out_fnam = in_fnam + '.lemmatized'
-  with open(in_fnam) as in_f:
-    with open(out_fnam, 'w') as out_f:
-      for line in in_f:
-        print(" ".join([wnl.lemmatize(w) for w in nltk.tokenize.word_tokenize(line)]), file = out_f)
+  with open(in_fnam, encoding = 'utf-8') as f:
+    doc = nlp(f.read())
+  if(args.strip_entities):
+    tokens = filter(lambda x: x.ent_type_ == '', doc)
+  else:
+    tokens = doc #bit ugly, but happens to work here
+  lemmas = [x.lemma_ for x in tokens]
+  with open(out_fnam, 'w', encoding = 'utf-8') as f:
+    print(" ".join(lemmas).strip(), file = f)
