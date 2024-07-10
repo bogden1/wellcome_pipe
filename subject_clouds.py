3d358a2f2dfba23d87b6306923dc434c96748f0d
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-07-10 10:49:10 +0100

    Report on missing subject in subject clouds

--- subject_clouds.py
+++ subject_clouds.py
@@ -8,6 +8,9 @@ import argparse
 import numpy as np
 import pandas as pd
 
+NO_SUBJECT_ID = '00000000'
+NO_SUBJECT_LABEL = '<No Subject>'
+
 sys.path.append(os.path.dirname(sys.argv[0]))
 import _wordcloud as wc
 
@@ -71,6 +74,11 @@ with open(args.doc_subjects) as f:
 
 with open(args.subject_labels) as f:
   subj_labels = json.load(f)
+if NO_SUBJECT_ID in subj_labels:
+  print('No subject indicator unexpectedly listed as a real subject id', file = sys.stderr)
+  sys.exit(1)
+else:
+  subj_labels[NO_SUBJECT_ID] = NO_SUBJECT_LABEL
 
 for topic_count in args.topic_counts:
   path = f'{args.output_dir}/{os.path.basename(args.prefix)}_{topic_count}'
@@ -90,6 +98,8 @@ for topic_count in args.topic_counts:
     #A list of subject ids for this doc
     #None: this doc id is in the doc->subjects mapping and has no subjects
     #NaN: this doc_id does not exist in the doc->subjects mapping (so I assume this is a bug earlier in the pipeline, generating a bad doc id)
+    #So convert None (but not NaN) into an indicator of that there is no subject
+    subj_proportions = subj_proportions.assign(subj_id = subj_proportions.subj_id.map(lambda x: NO_SUBJECT_ID if x is None else x))
 
     #create a single-col df, indexed by subject id, with proportion of that subject id in the topic (count of times that subject appeared, multiplied by the proportion of the document that corresponds to the topic)
     subj_proportions = subj_proportions.explode('subj_id') #explode lists of subjects out to separate rows
