d6b591d4733060152f4ddff9c992a6230f8d7c10
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-07-11 18:33:39 +0100

    Fixed up bar chart generation
    Cleaned up some display oddities and got linking to work properly
    with SVG files.
    
    Unfortunately Plotly's SVG generation seems a bit unreliable, so
    created svg_fixup.py to tidy up what comes out of Plotly. This
    included things like hiding the target id for the title in the
    title text itself, as the href was not surviving SVG generation.

--- meta_bars.py
+++ meta_bars.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
+#plotly export to SVG is a bit messy, especially where links are concerned.
+#Output of this script gets fixed up by svg_fixup.py
 
 import os
 import sys
 import json
 import pickle
 import argparse
-import textwrap
-import numpy as np
 import pandas as pd
 import plotly.express as px
 
@@ -17,9 +17,6 @@ parser.add_argument('topic_counts',
 parser.add_argument('--prefix',
                     default = '17xx_lemmatized_sorted',
                     help = 'Prefix for document topic composition file(s) e.g. to use "17xx_lemmatized_sorted-composition_*.txt", pass "17xx_lemmatized_sorted"')
-parser.add_argument('--doc-labels',
-                    default = 'data/corpora/example/normalized/titles.json',
-                    help = 'Location of doc id->title map (a decorate_subjects.py output)')
 parser.add_argument('--mallet-metadata',
                     required = True,
                     help = 'TSV file used for Mallet input')
@@ -55,13 +52,6 @@ else:
     pickle.dump(metadata, f)
 metadata = metadata.assign(cat_id = metadata['cat_id'].str.split().str[-1]) #get the Wellcome catalogue doc id, given current TSV format
 
-with open(args.doc_labels) as f:
-  #doc_labels = {}
-  full_doc_labels = {}
-  for k, v in json.load(f).items():
-    #doc_labels[k] = v['short']
-    full_doc_labels[k] = v['full']
-
 for topic_count in args.topic_counts:
   path = f'{args.output_dir}/{os.path.basename(args.prefix)}_{topic_count}'
   if not os.path.exists(path):
@@ -73,35 +63,18 @@ for topic_count in args.topic_counts:
   topics = list(range(0, n_topics))
   df = pd.read_csv(in_fnam, sep = '\t', index_col = 0, names = ['doc_id', 'author'] + topics).join(metadata, validate = '1:1')
 
-  #doc_topics_df = df.set_index(df.cat_id.map(full_doc_labels)).drop(['year', 'author'], axis = 1)
   doc_topics_df = df.set_index(df.cat_id).drop(['year', 'author', 'cat_id'], axis = 1)
-  #px.bar(doc_topics_df, title = 'Document contributions to topics', labels = {'index': 'Topic', 'value': 'Contribution'}).show()
   counter = 0
   for cat_id, row in doc_topics_df.iterrows():
     print(f'Writing chart for doc {counter}/{len(doc_topics_df)} ({cat_id})...', end = '')
-    if cat_id in full_doc_labels:
-      title = f'<a href="https://wellcomecollection.org/works/{cat_id}>{full_doc_labels[cat_id]} ({cat_id})</a>'
-    else:
-      title = f'<a href="https://wellcomecollection.org/works/{cat_id}">{cat_id}</a>'
-    #fig = px.bar(row, y = row * 100, text = f'<a href="https://example.com/topic_{row.index}_addr">{row.index}</a>', title = '<br>'.join(textwrap.wrap(title, 200)), range_y = [0, 100], labels = {'index': 'Topic', 'y': 'Contribution %'})
+    title = cat_id
     text = [f'<a href="https://example/com/topic_{x}">{x}</a>' for x in row.index.to_list()]
     text = [f'{x}' for x in row.index.to_list()]
-    fig = px.bar(row, y = row * 100, title = '<br>'.join(textwrap.wrap(title, 200)), range_y = [0, 100], labels = {'index': 'Topic', 'y': 'Contribution %'})#, text = text)
+    fig = px.bar(row, y = row * 100, title = title, range_y = [0, 100], labels = {'index': 'Topic', 'y': 'Topic %'})#, text = text)
     fig.update_layout(showlegend = False)
     for x, y in row.items():
-    #  print(x, y)
-      fig.add_annotation(x = x, y = y * 100, text = f'<a href="https://example.com/topic_{x}_addr">{x}</a>', showarrow = False, bgcolor = 'white')
-    #:fig.update_annotations(font = {'family': 'sans serif', 'size': 18, 'color': '#000000'})
-    #fig.add_annotation(
-    #    {
-    #        "x": r[1]["tag"],
-    #        "y": r[1]["val"],
-    #        "text": f"""<a href="{r[1]["url"]}" target="_blank">{r[1]["tag"]}</a>""",
-    #    }
-    #)
+      fig.add_annotation(x = x, y = y * 100, text = f'<a href="https:../topics.html#topic_{x}">{x}</a>', showarrow = False, yshift = 7, xshift = -1, textangle = -90, font_size = 8)
     
-    #fig.show()
-    #breakpoint()
-    fig.write_image(f'{args.output_dir}/{cat_id}.svg')
+    fig.write_image(f'{path}/{cat_id}.svg')
     print(' done')
     counter += 1
---
+++ svg_fixup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#Usage example:
+#find figures -type f -name '*.svg' | grep -v '/pp_[^/]*.svg' | sed 's#.*#./svg_fixup.py & --doc-labels titles.json#' | nice parallel
+import os
+import sys
+import json
+import argparse
+import xml.etree.ElementTree as ET
+
+skipped_files = 0
+skipped_links = 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infiles', nargs = '+')
+parser.add_argument('--doc-labels', default = 'data/corpora/example/normalized/titles.json')
+args = parser.parse_args()
+
+with open(args.doc_labels) as f:
+  doc_labels = json.load(f)
+
+WELLCOME='https://wellcomecollection.org/works/'
+SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
+XLINK_NAMESPACE = 'http://www.w3.org/1999/xlink'
+ET.register_namespace('', SVG_NAMESPACE)
+ET.register_namespace('xlink', XLINK_NAMESPACE)
+for file in args.infiles:
+  tree = ET.parse(file)
+
+  #use more space
+  root = tree.getroot()
+  root.set('width', '100%')
+  root.set('height', '100%')
+
+  #links to topics
+  for link in tree.findall('.//a[@xlink:href]', namespaces = {'': SVG_NAMESPACE, 'xlink': XLINK_NAMESPACE}):
+    link.set('{http://www.w3.org/1999/xlink}href', link.get('{http://www.w3.org/1999/xlink}href')[6:])
+
+  #link to doc
+  title = tree.find('./g[@class="infolayer"]/g[@class="g-gtitle"]', namespaces = {'': SVG_NAMESPACE, 'xlink': XLINK_NAMESPACE})
+  children = list(title)
+  if len(children) != 1: raise
+  text = children[0]
+  if text.tag != f'{{{SVG_NAMESPACE}}}text': raise
+  title.remove(text)
+  ET.SubElement(title, 'a', {'xlink:href': f'{WELLCOME}/{text.text}'}).append(text)
+  if text.text in doc_labels:
+    text.text=f'{doc_labels[text.text]["full"]} ({text.text})'
+  text.set('style', "font-family: 'Open Sans', verdana, arial, sans-serif; font-size: 10px; fill: rgb(42, 63, 95); opacity: 1; white-space: pre;")
+
+  #smaller annotations -- commented out as I managed to get meta_bars to generate something sensible, but keeping for reference
+  #for annotation in tree.findall('//text[@class="annotation-text"]', namespaces = {'': SVG_NAMESPACE, 'xlink': XLINK_NAMESPACE}):
+  #  annotation.set('style', "font-family: 'Open Sans', verdana, arial, sans-serif; font-size: 8px; fill: rgb(0,0,0); fill-opacity: 1; white-space: pre; pointer-events: all;")
+  #for annotation in tree.findall('//g[@class="annotation-text-g"]', namespaces = {'': SVG_NAMESPACE, 'xlink': XLINK_NAMESPACE}):
+    #rot, x, y = annotation.get('transform')[7:-1].split(',')
+    #annotation.set('transform', f'rotate(-90,{x},{y})')
+
+  tree.write(os.path.dirname(file) + '/' + 'pp_' + os.path.basename(file))
+
+  if skipped_links > 0:
+    print('Skipped {skipped_links} links', file = sys.stderr)
+  if skipped_files > 0:
+    print('Skipped {skipped_files} files', file = sys.stderr)
