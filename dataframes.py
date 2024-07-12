2b4eadbf65a8163a980be9e61a17525a07eb1f8d
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-07-12 17:13:38 +0100

    Add timestamp checking to dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -7,9 +7,12 @@ def save(fnam, data):
   with open(fnam, 'wb') as f:
     pickle.dump(data, f)
 
-def load(fnam):
+def load(fnam, base = None):
   if args.force:
     return None
+  if not base is None:
+    if os.path.getmtime(base) > os.path.getmtime(fnam):
+      return None
   if os.path.exists(fnam):
     with open(fnam, 'rb') as f:
       d = pickle.load(f)
@@ -30,7 +33,7 @@ sums_df_name   = base_name + '_sums.df'
 freq_df_name   = base_name + '_freqs.df'
 freq_csv_name  = base_name + '_freqs.csv'
 
-base_df = load(base_df_name)
+base_df = load(base_df_name, base_name + ext)
 if base_df is None:
   print(f'constructing {base_df_name}')
   base_df = pd.read_csv(base_name + ext, sep = ' ', names = ('doc', 'source', 'pos', 'typeindex', 'type', 'topic'), skiprows = 3)
@@ -43,7 +46,7 @@ if base_df is None:
 #4      1944
 #7        21
 #9      4917
-counts_df = load(counts_df_name)
+counts_df = load(counts_df_name, base_name + ext)
 if counts_df is None:
   print(f'constructing {counts_df_name}')
   assert base_df['doc'].isna().sum() == 0
@@ -52,7 +55,7 @@ if counts_df is None:
 
 #Total words in topic (counting duplicates e.g. if "elixir" appears 1944 times, all 1944 will be counted)
 #e.g. at time of writing, topic 4 is 24359667; topic 7 is 9549174 and topic 9 is 9010839
-sums_df = load(sums_df_name)
+sums_df = load(sums_df_name, base_name + ext)
 if sums_df is None:
   print(f'constructing {sums_df_name}')
   assert base_df['doc'].isna().sum() == 0
@@ -70,7 +73,7 @@ if sums_df is None:
 #4     0.000080
 #7     0.000002
 #9     0.000546
-freq_df = load(freq_df_name)
+freq_df = load(freq_df_name, base_name + ext)
 if freq_df is None:
   print(f'constructing {freq_df_name}')
   freq_df = counts_df.div(sums_df, level = 'topic', axis = 0)
