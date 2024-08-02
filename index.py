1c284ab8322939e312fbce71c6a68c66c87ffa22
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-08-02 08:35:52 +0100

    Home page as table

--- index.py
+++ index.py
@@ -17,21 +17,21 @@ root = sys.argv[1]
 with open(f'{root}/index.html', 'w') as index_handle:
   print('<html><body>', file = index_handle)
   print('<h3>Topics</h3>', file = index_handle)
+  print('<table>', file = index_handle)
   for d in sorted(filter(lambda x: '_' in x, os.listdir(root)), key = lambda x: int(x.split('_')[-1])):
     if d in table:
       topics_name = table[d]
     else:
       topics_name = d
-    print('<ul>', file = index_handle)
     if os.path.exists(f'{root}/{d}/topics.html'):
-      print(f'<li>{topics_name}<a target="_blank" href="{d}/topics.html">Topics</a> <a href="{d}_documents.html">Documents</a></li>', file = index_handle)
+      print(f'<tr><td>{topics_name}</td><td><a target="_blank" href="{d}/topics.html">Topics</a></td><td><a href="{d}/documents.html">Documents</a></td></tr>', file = index_handle)
       for titles_file in sys.argv[2:]:
         with open(titles_file) as tf_handle:
           titles = json.load(tf_handle)
         mapping = {titles[x]['full']: x for x in titles}
-        with open(f'{root}/{d}_documents.html', 'w') as docs_handle:
+        with open(f'{root}/{d}/documents.html', 'w') as docs_handle:
           print(f'<html><body><h3>All documents in {d}</h3><ul>', file = docs_handle)
           for title in sorted(mapping.keys(), key = lambda x: x.translate(str.maketrans('','',string.punctuation))):
             print(f'<li><a target="_blank" href="{d}/figures/pp_{mapping[title]}.svg">[{topics_name}] {title}</a></li>', file = docs_handle)
           print('</ul></body></html>', file = docs_handle)
-    print('</ul>', file = index_handle)
+  print('</table>', file = index_handle)
