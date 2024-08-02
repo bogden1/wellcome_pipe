605e7b8c8c21f5e9aab43b91ea5ab8ababed366f
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-08-02 08:23:19 +0100

    More explict handle name

--- index.py
+++ index.py
@@ -14,23 +14,23 @@ for x in (10, 50, 100, 250, 500):
 
 root = sys.argv[1]
 
-with open(f'{root}/index.html', 'w') as f:
-  print('<html><body>', file = f)
-  print('<h3>Topics</h3>', file = f)
+with open(f'{root}/index.html', 'w') as index_handle:
+  print('<html><body>', file = index_handle)
+  print('<h3>Topics</h3>', file = index_handle)
   for d in sorted(filter(lambda x: '_' in x, os.listdir(root)), key = lambda x: int(x.split('_')[-1])):
     if d in table:
       topics_name = table[d]
     else:
       topics_name = d
     if os.path.exists(f'{root}/{d}/topics.html'):
-      print(f'<details>', file =f)
-      print(f'<summary style="cursor: pointer"><a target="_blank" href="{d}/topics.html">{topics_name}</summary>', file = f)
-      print('<ul>', file = f)
+      print(f'<details>', file = index_handle)
+      print(f'<summary style="cursor: pointer"><a target="_blank" href="{d}/topics.html">{topics_name}</summary>', file = index_handle)
+      print('<ul>', file = index_handle)
       for titles_file in sys.argv[2:]:
         with open(titles_file) as tf_handle:
           titles = json.load(tf_handle)
         mapping = {titles[x]['full']: x for x in titles}
         for title in sorted(mapping.keys(), key = lambda x: x.translate(str.maketrans('','',string.punctuation))):
-          print(f'<li><a target="_blank" href="{d}/figures/pp_{mapping[title]}.svg">[{topics_name}] {title}</a></li>', file = f)
-      print('</ul>', file = f)
-      print('</details>', file = f)
+          print(f'<li><a target="_blank" href="{d}/figures/pp_{mapping[title]}.svg">[{topics_name}] {title}</a></li>', file = index_handle)
+      print('</ul>', file = index_handle)
+      print('</details>', file = index_handle)
