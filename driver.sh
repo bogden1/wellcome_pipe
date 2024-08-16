9332bf2906c827201fc7788f8ae79d6275c9c2c6
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-08-16 12:06:55 +0100

    Add stemmed build to driver.sh

--- driver.sh
+++ driver.sh
@@ -38,6 +38,11 @@ build() {
 for x in 10 50 100 250 500; do
   build 17xx_rebuild                        "${x}"
   build 17xx_rebuild_entitystripped         "${x}" 'STRIP_ENTITIES=--strip-entities'
+  build 17xx_rebuild_stemmed                "${x}"                                   'TRUNC_SUFFIX=stemmed'
+
+  #In principle this would make sense, but I'd first have to rewrite the stemmer to be able to strip entities
+  #Or make entity stripping independent of truncation
+  #build 17xx_rebuild_stemmed_entitystripped "${x}" 'STRIP_ENTITIES=--strip-entities' 'TRUNC_SUFFIX=stemmed'
 done
 
 #index has to run after everything else is finished (or at least after all of the _title.json files are generated)
